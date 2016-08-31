7cc04a384f9b5e9e38e24bab4fb8422925756c8b
ryan.cao <ryan.cao@airparking.com>
2016-08-31 11:42:53 +0800

    add loadbalance for remote service

--- src/main/java/sample/web/ui/AppAccessInvoker.java
+++ src/main/java/sample/web/ui/AppAccessInvoker.java
@@ -13,6 +13,9 @@ import com.airparking.core.comm.utils.Signature;
 import com.s515.rpc.invoker.http.HttpInvoker;
 
 public class AppAccessInvoker extends HttpInvoker {
+	private String appId;
+	private String secretKey;
+	private String version;
 
 	@Override
 	public List<NameValuePair> buildValuePair(Map<String, Object> params)
@@ -27,20 +30,40 @@ public class AppAccessInvoker extends HttpInvoker {
         for (Map.Entry<String, Object> entry : params.entrySet()) {
             nvps.add(new BasicNameValuePair(entry.getKey(), entry.getValue().toString()));
         }
-        nvps.add(new BasicNameValuePair("sign", Signature.sign(params, "f7ec49c46ff8b0349f97cd4a8737d471")));
+        nvps.add(new BasicNameValuePair("sign", Signature.sign(params, this.secretKey)));
         return nvps;
 	}
 	
 	public void addCommonParameters(Map<String, Object> params) {
-		params.put("appId", "apyAqKqNs2qxWZPWOQ");
+		params.put("appId", this.appId);
 		params.put("appVersion", "1.0");
 		params.put("timestamp", System.currentTimeMillis());
-		params.put("deviceId", "e0a83343b86f6595a32");
 		params.put("clientType", 1);
-		params.put("version", "1.0.0");
-		params.put("userLng", 113.320519);
-		params.put("userLat", 23.12248);
-		params.put("token", "klt57KHjyyyVYLMUFhZKu1SbYEwXUgXC");
+		params.put("version", this.version);
+	}
+
+	public String getAppId() {
+		return appId;
+	}
+
+	public void setAppId(String appId) {
+		this.appId = appId;
+	}
+
+	public String getSecretKey() {
+		return secretKey;
+	}
+
+	public void setSecretKey(String secretKey) {
+		this.secretKey = secretKey;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
 	}
 	
 }
--- src/main/java/sample/web/ui/ServiceConfig.java
+++ src/main/java/sample/web/ui/ServiceConfig.java
@@ -1,17 +1,37 @@
 package sample.web.ui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 
 import com.s515.rpc.annotation.ServiceScan;
 import com.s515.rpc.invoker.Invoker;
+import com.s515.rpc.router.lbs.RandomLoadBalance;
+import com.s515.rpc.router.server.Host;
+import com.s515.rpc.router.server.HttpNode;
+import com.s515.rpc.router.server.Node;
 
 @Configuration
 @ServiceScan(basePackages = {"sample.web.ui.service"})
 public class ServiceConfig {
+	@Value("${app.nodes.appId}")
+	private String appId;
+	@Value("${app.nodes.secretKey}")
+	private String secretKey;
+	@Value("${app.nodes.version}")
+	private String version;
+	@Value("${app.nodes}")
+	private String appNodes;
+	@Value("${app.name}")
+	private String appName;
+	
 	@Bean(name = "httpClient")
 	public CloseableHttpClient closeableHttpClient() {
 		return HttpClients.createDefault();
@@ -21,6 +41,32 @@ public class ServiceConfig {
 	public Invoker invoker(@Qualifier("httpClient") CloseableHttpClient httpClient) {
 		AppAccessInvoker invoker = new AppAccessInvoker();
 		invoker.setHttpClient(httpClient);
+		invoker.setAppId(appId);
+		invoker.setSecretKey(secretKey);
+		invoker.setVersion(version);
+		
+		invoker.setLoadBalance(new RandomLoadBalance());
+		invoker.setNodes(getAppNodes());
 		return invoker;
+	}
+	
+	private List<Node> getAppNodes() {
+		if (!StringUtils.isEmpty(appNodes)) {
+			String[] nodes = appNodes.split(",");
+			
+			final List<Node> appNodes = new ArrayList<>();
+			
+			String[] nodeUrl;
+			Node n;
+			for (String node : nodes) {
+				nodeUrl = node.trim().split(":");
+				n = new HttpNode(appName, new Host(nodeUrl[0], nodeUrl.length > 1 ? Integer.valueOf(nodeUrl[1]) : 80));
+				appNodes.add(n);
+			}
+			
+			return appNodes;
+		}
+		
+		return null;
 	}
 }
